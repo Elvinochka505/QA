f4fade35ce8946b5a9dcec587e33b6c936862ddd
Elvina <habibullinaelvina1@gmail.com>
2025-11-17 17:41:18 +0500

    1

---
+++ src/test/java/AppCardDeliveryTaskOneTest.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.Keys;
+import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+
+public class AppCardDeliveryTaskOneTest {
+
+    @BeforeEach
+    void setup() {
+        Selenide.open("http://localhost:9999");
+    }
+
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+
+    }
+
+    @Test
+    public void shouldBeSuccessfullyCompleted() {
+        $("[data-test-id= 'city'] input").setValue("Рязань");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Макаров Сергей");
+        $("[data-test-id='phone'] input").setValue("+79051234545");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $( ".notifacion__content")
+        .should(Condition.visible, Duration. ofSeconds(15))
+        .should(Condition.text("Встреча успешно забронирована на " + planningDate));
+    }
+
+}
+
+
+
+
+
